89c03b451b55e6ad6c848454282c76f47477ccd6
Omid <>
2023-08-19 12:24:38 +0330

    create router

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const router = require("./src/routes")
 
 mongoose.connect("mongodb://127.0.0.1:27017/samane")
 .then(()=>console.log("connected."))
 .catch(()=>console.log("couldn't connect."))
 
-
+router('/',router);
 
 const port = process.env.PORT || 3000;
 
