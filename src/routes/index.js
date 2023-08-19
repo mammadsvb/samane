2dc95ffe2c405600270d82b5a3f12c61373b90fc
Omid <>
2023-08-19 16:09:29 +0330

    create login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const registerRouter = require("./register")
+const registerRouter = require("./register");
+const loginRouter = require("./login");
 
-router.use('/register',registerRouter)
+router.use('/register',registerRouter);
+router.use("/login",loginRouter);
 
 
 module.exports = router;
---
+++ src/routes/login/index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router();
+
+const controller = require("./controller");
+const validator = require("./validator"); 
+
+router.get("/");
+
+
+router.post("/");
+
+
+module.exports = router;
