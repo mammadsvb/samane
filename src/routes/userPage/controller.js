4a612b5bd1757319abcf4192eaf8f2cc4a0ede06
Omid <>
2023-08-20 14:06:22 +0330

    upload file

--- src/routes/userPage/controller.js
+++ src/routes/userPage/controller.js
@@ -1,8 +1,34 @@
 const Conteroller = require("../controller");
+const multer = require("multer");
+const mkdir = require('mkdirp');
 
 module.exports = new class extends Conteroller{
 
     loadPage(req,res){
         res.render("user");
     }
+
+    getFile(){
+        const storage = multer.diskStorage({
+            destination: function (req, file, cb) {
+                // mkdir('./public/uploads').then(made=>{
+                //     cb(null, './public/uploads');
+                // })
+                cb(null, './public/uploads');
+            },
+            filename: function (req, file, cb) {
+              cb(null, Date.now() + '-' + file.filename )
+            }
+        })
+           
+        const upload = multer({ storage: storage });
+          
+          return upload;      
+    }
+
+    sayHello(req,res){
+        console.log(req.file);
+        res.redirect("user");
+    }
+    
 }
--- src/routes/userPage/index.js
+++ src/routes/userPage/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./controller")
+const controller = require("./controller");
+// const uploadUserProfile = require('../../middlewares/upload');
 
 router.get('/',
-controller.loadPage.bind(controller))
+controller.loadPage.bind(controller));
+
+router.post("/",
+controller.getFile().single("filename"),
+controller.sayHello.bind(controller));
 
 
 module.exports = router;
