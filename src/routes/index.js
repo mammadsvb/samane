322aba43e625ce0588230cdfe88fa64ffeaf5dfb
Omid <>
2023-08-20 11:21:28 +0330

    create user page

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {isLoggedin,loginPage,isAdmin} = require('../middlewares/user')
 const registerRouter = require("./register");
 const loginRouter = require("./login");
-const adminRouter = require("./admin")
+const userRouter = require("./userPage");
+const adminRouter = require("./admin");
 
 router.use('/register',registerRouter);
 router.use("/login",loginPage,loginRouter);
+router.use("/user",isLoggedin,userRouter);
 router.use("/admin",isLoggedin,isAdmin,adminRouter);
 
 
---
+++ src/routes/userPage/controller.js
@@ -0,0 +1,5 @@
+const Conteroller = require("../controller");
+
+module.exports = new class extends Conteroller{
+
+}
