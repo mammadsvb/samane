1d8a8275a367fc1647969e44e65bea5165626881
Omid <>
2023-08-20 11:03:52 +0330

    token login

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -22,10 +22,28 @@ async function isLoggedin(req,res,next){
         console.error(err);
         res.redirect("login");
     }
+}
 
+async function loginPage(req,res,next){
 
+    const token = req.signedCookies.id;
+    console.log(token)
+    if(!token){
+        return next();
+    }
+
+    try{
+        const decode = jwt.verify(token,config.get("token-key"));
+
+        const user = await User.findById(decode.id);
+        req.user = user;
+        res.redirect("admin")
+    }catch(err){
+        next();
+    }
 }
 
 module.exports = {
-    isLoggedin
+    isLoggedin,
+    loginPage
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {isLoggedin} = require('../middlewares/user')
+const {isLoggedin,loginPage} = require('../middlewares/user')
 const registerRouter = require("./register");
 const loginRouter = require("./login");
 const adminRouter = require("./admin")
 
 router.use('/register',registerRouter);
-router.use("/login",loginRouter);
+router.use("/login",loginPage,loginRouter);
 router.use("/admin",isLoggedin,adminRouter)
 
 
